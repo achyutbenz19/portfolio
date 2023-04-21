6fffc026be9daf1cec30601a95bea9c4e3606370
achyutbenz19 <achyut.benz@gmail.com>
2023-04-21 12:14:43 -0400

    contact page done

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -36,18 +36,26 @@ const Contact = () => {
     });
   };
   
-
   return (
     <div className="contact">
       <h1>Contact Me!</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
+        <a href='https://github.com/achyutbenz19'>
+            <img src={require("./logos/Github-logo.png")} alt="Github-Logo" className="Github-Logo"/>
+        </a>
         <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
 
         <label htmlFor="email">Email:</label>
+        <a href='https://www.linkedin.com/in/achyut-byanjankar/'>
+            <img src={require("./logos/linkedin-logo.png")} alt="Linkedin-Logo" className="Linkedin-Logo"/>
+        </a> 
         <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
 
         <label htmlFor="message">Message:</label>
+        <a href='mailto:achyut.benz@gmail.com'>
+            <img src={require("./logos/Gmail-Logo.png")} alt="Gmail-Logo" className="Gmail-Logo"/>
+        </a>
         <textarea id="message" name="message" value={formData.message} onChange={handleChange} required></textarea>
 
         <button type="submit">Submit</button>
