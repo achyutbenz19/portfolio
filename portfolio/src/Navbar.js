54765fabbe14705d77580a6489b1f9cfd1696962
achyutbenz19 <achyut.benz@gmail.com>
2023-05-17 09:12:27 -0500

    added containers

--- portfolio/src/Navbar.js
+++ portfolio/src/Navbar.js
@@ -1,13 +1,13 @@
 const Navbar = () => {
   return (
-      <nav className="navbar">
-        <div className="links">
-          <a href="/">home</a>
-          <a href="/expertise">expertise</a>
-          <a href="/work">work</a>
-          <a href="/contact">contact</a>
-        </div>
-      </nav>
+    <nav className="navbar">
+      <div className="links">
+        <a href="/">home</a>
+        <a href="/expertise">expertise</a>
+        <a href="/work">work</a>
+        <a href="/contact">contact</a>
+      </div>
+    </nav>
   );
 }
 
--- portfolio/src/Work.js
+++ portfolio/src/Work.js
@@ -1,8 +1,34 @@
 const Work = () => {
     return ( 
         <div className="work">
+        <div className="work-title">
             <h1>My Work</h1>
         </div>
+        <div className="work-container">
+          <h4><b>24/7 Operable Market</b></h4>
+          <p className="work-p">Colony is a 24/7 operable marketplace, connecting investors with fractional real estate investments, enabling them to buy and sell anytime, anywhere.</p>
+        </div>
+        <div className="work-container">
+          <h4><b>Automated Investing</b></h4>
+          <p className="work-p">Colony’s automated investing platform allows users to connect their bank accounts and automate investments from the comfort of their own home, making it easier for them to take advantage of real estate opportunities.</p>
+        </div>
+        <div className="work-container">
+          <h4><b>No Minimums</b></h4>
+          <p className="work-p">Colony offers no minimums on investments, allowing users to invest with as little as $10, making it accessible to everyone.</p>
+        </div>
+        <div className="work-container-2">
+          <h4><b>Market Insights</b></h4>
+          <p className="work-p">Colony provides users with real-time market insights, powered by data-driven analytics, enabling them to make more informed decisions about their investments.</p>
+        </div>
+        <div className="work-container-2">
+          <h4><b>Tax Optimization</b></h4>
+          <p className="work-p">Colony’s tax optimization feature allows users to optimize their investments for tax purposes, helping them maximize their returns and minimize their taxes.</p>
+        </div>
+        <div className="work-container-2">
+          <h4><b>Investor Education</b></h4>
+          <p className="work-p">Colony provides users with educational materials and resources to help them learn more about real estate investing, enabling them to make more informed decisions.</p>
+        </div>
+      </div>
      );
 }
  
